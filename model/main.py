c76ddbacb6d139cf2b2c338c2a5e86df0b53b042
Quentin-qha <haentjensquentin@gmail.com>
2025-08-14 16:42:36 +0000

    get theme in .py

--- model/main.py
+++ model/main.py
@@ -4,6 +4,7 @@ from extractor import extract_text_from_pdf
 from tokenizer import chunk_text, count_tokens
 from embedder import get_embeddings
 from chroma_handler import save_to_chroma
+from clustering_theme import hdbscan_clustering
 from quiz_generator import generate_quiz_from_chunks
 import requests
 
@@ -17,26 +18,30 @@ def main(difficulty="standard"):
     # 3. Chunker
     chunks = chunk_text(text)
     token_counts = [count_tokens(c) for c in chunks]
-    print(f"{len(chunks)} chunks. Moy tokens: {round(sum(token_counts)/len(token_counts))}")
+    #print(f"{len(chunks)} chunks. Moy tokens: {round(sum(token_counts)/len(token_counts))}")
 
     # 4. Embeddings
     embeddings = get_embeddings(chunks, EMBEDDING_MODEL_NAME)
-    print(f"Embeddings shape: {embeddings.shape}")
+    #print(f"Embeddings shape: {embeddings.shape}")
+
+    # Clustering
+    themes = hdbscan_clustering(chunks)
+    print("thèmes trouvé:", themes)
 
     # 5. Stockage Chroma
     chroma_db = save_to_chroma(chunks, EMBEDDING_MODEL_NAME, CHROMA_DB_PATH)
     
     # 6. Récupération de la vector db
     vector_data = chroma_db.get()
-    query = "Qui est Napoléon ?"
+    #query = "Qui est Napoléon ?"
     
-    retriever = chroma_db.as_retriever(
-        search_type = "similarity_score_threshold",
-        search_kwargs = {"k": 3, "score_threshold": 0.2},
-    )
+    #retriever = chroma_db.as_retriever(
+    #    search_type = "similarity_score_threshold",
+    #    search_kwargs = {"k": 3, "score_threshold": 0.2},
+    #)
 
-    elevant_docs = retriever.invoke(query)
-    print(elevant_docs)
+    #elevant_docs = retriever.invoke(query)
+    #print(elevant_docs)
     #quiz = generate_quiz_from_chunks(vector_data, difficulty)
 
     # 7. Envoie à l'API
